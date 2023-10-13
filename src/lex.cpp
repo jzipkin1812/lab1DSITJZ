44cf28107561cf46eab08d3f8beda70a1a669a88
Dalia Sebat <daliasebat@cs.ucsb.edu>
2023-10-13 00:06:22 -0700

    fdsaf

--- src/lex.cpp
+++ src/lex.cpp
@@ -22,8 +22,7 @@ Lexer::Lexer()
     int lineNumber = 0;
     // int colNumber = 0;
     string currentString = "";
-
-    do
+    while (!cin.eof() && expression != "")
     {
         // cout << expression << endl;
         lineNumber++;
@@ -108,7 +107,88 @@ Lexer::Lexer()
             currentString = "";
         }
         getline(cin, expression);
-    } while (cin.good() && expression != "");
+    }
+    lineNumber++;
+    for (int i = 0; i < (int)expression.length(); i++)
+    {
+        try
+        {
+            // colNumber++;
+            char currentChar = expression[i];
+            // cout << "CURRENT CHAR: " << currentChar << endl;
+            switch (currentChar)
+            {
+            /*case '\n':
+                if (currentString != "")
+                {
+                    tokens.push_back(Token(lineNumber, colNumber - currentString.length(), currentString));
+                    currentString = "";
+                }
+                lineNumber++;
+                colNumber = 0;
+                continue;*/
+            case ')':
+            case '(':
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                if (currentString != "")
+                {
+                    tokens.push_back(Token(lineNumber, i + 1 - currentString.length(), currentString));
+                    currentString = "";
+                }
+                currentString = currentChar;
+                tokens.push_back(Token(lineNumber, i + 1, currentString));
+                currentString = "";
+                continue;
+            default:
+                if (isspace(currentChar))
+                {
+                    if (currentString != "")
+                    {
+                        tokens.push_back(Token(lineNumber, i + 1 - currentString.length(), currentString));
+                        currentString = "";
+                    }
+                    continue;
+                }
+                if (currentChar == '.')
+                {
+                    // cout << colNumber << endl;
+                    // cout << currentString << endl;
+                    if ((int)expression.length() == i + 1 || !isdigit(expression[i + 1]))
+                    {
+                        // colNumber++;
+                        throw(i + 2);
+                    }
+                    if (currentString == "" || (int)currentString.find('.') != -1)
+                    {
+                        throw(i + 1);
+                    }
+                }
+                // cout << " IS DIGIT: " << isdigit(currentChar) << endl;
+                if (isdigit(currentChar) || currentChar == '.')
+                {
+                    currentString += currentChar;
+                    continue;
+                }
+                else
+                {
+                    throw(i + 1);
+                }
+            }
+        }
+        catch (int columnNum)
+        {
+            cout << "Syntax error on line " << lineNumber << " column " << columnNum << "." << endl;
+            exit(1);
+        }
+    }
+    if (currentString != "")
+    {
+        tokens.push_back(Token(lineNumber, expression.length() + 1 - currentString.length(), currentString));
+        currentString = "";
+    }
     tokens.push_back(Token(lineNumber + 1, expression.length() + 1, "END"));
 }
 
