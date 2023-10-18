6a6b3587064fcb3e7c923193542fa7e9ead125af
Javin Zipkin <javinzipkin@ucsb.edu>
2023-10-18 16:25:15 -0700

    trying to see input

--- src/parse.cpp
+++ src/parse.cpp
@@ -10,6 +10,11 @@ using namespace std;
 int main()
 {
     Lexer myLexer = Lexer();
+    for(Token t : myLexer.getTokens()[0])
+    {
+        cout << "[" << t.text << "]";
+    }
+    cout << endl;
     Parser myParser = Parser(myLexer.getTokens());
     myParser.print();
 }
