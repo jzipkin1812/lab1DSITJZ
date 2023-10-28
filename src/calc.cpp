479ca3f068d8ad163dde5ee080f2b30e376c29b9
Javin Zipkin <javinzipkin@ucsb.edu>
2023-10-28 13:56:12 -0700

    Trying to see errors

--- src/calc.cpp
+++ src/calc.cpp
@@ -10,6 +10,14 @@ using namespace std;
 int main()
 {
     Lexer myLexer = Lexer(true);
+    for(vector<Token> line : myLexer.getTokens())
+    {
+        for(Token t : line)
+        {
+            cout << t.text << " ";
+        }
+        cout << endl;
+    }
     Parser myParser = Parser(myLexer.getTokens());
     myParser.print(); // calls evaluate() internally.
     return 0;
